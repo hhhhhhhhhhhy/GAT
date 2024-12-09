a3c42d8ed63f9090745e06849e4c4bb33329be6d
ubuntu <ubuntu@localhost.localdomain>
2024-12-09 22:45:38 +0800

    update pubmed execution

---
+++ execute_pubmed.py
@@ -0,0 +1,212 @@
+# 导入必要的库
+import time
+import numpy as np
+import tensorflow as tf
+
+# 导入图注意力网络模型和数据处理工具
+from models import GAT
+from utils import process
+
+# 预训练模型的文件路径
+checkpt_file = 'pre_trained/pubmed/mod_pubmed.ckpt'
+
+# 数据集名称
+dataset = 'pubmed'
+
+# 训练参数
+batch_size = 1  # 批量大小
+nb_epochs = 100000  # 训练周期数
+patience = 100  # 早停耐心参数
+lr = 0.005  # 学习率
+l2_coef = 0.0005  # L2正则化系数
+hid_units = [8]  # 每个注意力头的隐藏单元数
+n_heads = [8, 1]  # 注意力头数，输出层额外增加一个
+residual = False  # 是否使用残差连接
+nonlinearity = tf.nn.elu  # 非线性激活函数
+model = GAT  # 使用的模型
+
+# 打印训练和架构超参数
+print('Dataset: ' + dataset)
+print('----- Opt. hyperparams -----')
+print('lr: ' + str(lr))
+print('l2_coef: ' + str(l2_coef))
+print('----- Archi. hyperparams -----')
+print('nb. layers: ' + str(len(hid_units)))
+print('nb. units per layer: ' + str(hid_units))
+print('nb. attention heads: ' + str(n_heads))
+print('residual: ' + str(residual))
+print('nonlinearity: ' + str(nonlinearity))
+print('model: ' + str(model))
+
+# 加载数据
+adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = process.load_data(dataset)
+features, spars = process.preprocess_features(features)
+
+# 获取节点数、特征大小和类别数
+nb_nodes = features.shape[0]
+ft_size = features.shape[1]
+nb_classes = y_train.shape[1]
+
+# 将邻接矩阵转换为密集格式
+adj = adj.todense()
+
+# 增加一个新的维度以适应批量大小
+features = features[np.newaxis]
+adj = adj[np.newaxis]
+y_train = y_train[np.newaxis]
+y_val = y_val[np.newaxis]
+y_test = y_test[np.newaxis]
+train_mask = train_mask[np.newaxis]
+val_mask = val_mask[np.newaxis]
+test_mask = test_mask[np.newaxis]
+
+# 将邻接矩阵转换为偏差矩阵
+biases = process.adj_to_bias(adj, [nb_nodes], nhood=1)
+
+# 创建TensorFlow图和会话
+with tf.Graph().as_default():
+    with tf.name_scope('input'):
+        # 定义输入占位符
+        ftr_in = tf.placeholder(dtype=tf.float32, shape=(batch_size, nb_nodes, ft_size))
+        bias_in = tf.placeholder(dtype=tf.float32, shape=(batch_size, nb_nodes, nb_nodes))
+        lbl_in = tf.placeholder(dtype=tf.int32, shape=(batch_size, nb_nodes, nb_classes))
+        msk_in = tf.placeholder(dtype=tf.int32, shape=(batch_size, nb_nodes))
+        attn_drop = tf.placeholder(dtype=tf.float32, shape=())
+        ffd_drop = tf.placeholder(dtype=tf.float32, shape=())
+        is_train = tf.placeholder(dtype=tf.bool, shape=())
+
+    # 构建模型并计算输出
+    logits = model.inference(ftr_in, nb_classes, nb_nodes, is_train,
+                                attn_drop, ffd_drop,
+                                bias_mat=bias_in,
+                                hid_units=hid_units, n_heads=n_heads,
+                                residual=residual, activation=nonlinearity)
+    # 调整输出形状以计算损失和准确率
+    log_resh = tf.reshape(logits, [-1, nb_classes])
+    lab_resh = tf.reshape(lbl_in, [-1, nb_classes])
+    msk_resh = tf.reshape(msk_in, [-1])
+    loss = model.masked_softmax_cross_entropy(log_resh, lab_resh, msk_resh)
+    accuracy = model.masked_accuracy(log_resh, lab_resh, msk_resh)
+
+    # 定义训练操作
+    train_op = model.training(loss, lr, l2_coef)
+
+    # 定义保存模型的操作
+    saver = tf.train.Saver()
+
+    # 初始化全局变量和局部变量
+    init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+    # 初始化验证损失和准确率的最小值和最大值
+    vlss_mn = np.inf
+    vacc_mx = 0.0
+    curr_step = 0
+
+    # 创建会话并初始化变量
+    with tf.Session() as sess:
+        sess.run(init_op)
+
+        # 初始化训练损失和准确率的平均值
+        train_loss_avg = 0
+        train_acc_avg = 0
+        val_loss_avg = 0
+        val_acc_avg = 0
+
+        # 训练模型
+        intial_t = time.time()
+
+        for epoch in range(nb_epochs):
+            t = time.time()
+            tr_step = 0
+            tr_size = features.shape[0]
+
+            # 训练一个epoch
+            while tr_step * batch_size < tr_size:
+                # 运行训练操作并计算损失和准确率
+                _, loss_value_tr, acc_tr = sess.run([train_op, loss, accuracy],
+                    feed_dict={
+                        ftr_in: features[tr_step*batch_size:(tr_step+1)*batch_size],
+                        bias_in: biases[tr_step*batch_size:(tr_step+1)*batch_size],
+                        lbl_in: y_train[tr_step*batch_size:(tr_step+1)*batch_size],
+                        msk_in: train_mask[tr_step*batch_size:(tr_step+1)*batch_size],
+                        is_train: True,
+                        attn_drop: 0.6, ffd_drop: 0.6})
+                train_loss_avg += loss_value_tr
+                train_acc_avg += acc_tr
+                tr_step += 1
+
+            # 验证模型
+            vl_step = 0
+            vl_size = features.shape[0]
+
+            while vl_step * batch_size < vl_size:
+                loss_value_vl, acc_vl = sess.run([loss, accuracy],
+                    feed_dict={
+                        ftr_in: features[vl_step*batch_size:(vl_step+1)*batch_size],
+                        bias_in: biases[vl_step*batch_size:(vl_step+1)*batch_size],
+                        lbl_in: y_val[vl_step*batch_size:(vl_step+1)*batch_size],
+                        msk_in: val_mask[vl_step*batch_size:(vl_step+1)*batch_size],
+                        is_train: False,
+                        attn_drop: 0.0, ffd_drop: 0.0})
+                val_loss_avg += loss_value_vl
+                val_acc_avg += acc_vl
+                vl_step += 1
+
+            # 打印训练和验证的结果
+            print('Training: loss = %.5f, acc = %.5f | Val: loss = %.5f, acc = %.5f, time = %.5f' %
+                    (train_loss_avg/tr_step, train_acc_avg/tr_step,
+                    val_loss_avg/vl_step, val_acc_avg/vl_step, time.time() - t))
+
+            # 检查是否满足早停条件
+            if val_acc_avg/vl_step >= vacc_mx or val_loss_avg/vl_step <= vlss_mn:
+                if val_acc_avg/vl_step >= vacc_mx and val_loss_avg/vl_step <= vlss_mn:
+                    vacc_early_model = val_acc_avg/vl_step
+                    vlss_early_model = val_loss_avg/vl_step
+                    saver.save(sess, checkpt_file)
+                vacc_mx = np.max((val_acc_avg/vl_step, vacc_mx))
+                vlss_mn = np.min((val_loss_avg/vl_step, vlss_mn))
+                curr_step = 0
+            else:
+                curr_step += 1
+                if curr_step == patience:
+                    print('Early stop! Min loss: ', vlss_mn, ', Max accuracy: ', vacc_mx)
+                    print('Early stop model validation loss: ', vlss_early_model, ', accuracy: ', vacc_early_model)
+                    break
+
+            # 重置训练和验证损失和准确率的平均值
+            train_loss_avg = 0
+            train_acc_avg = 0
+            val_loss_avg = 0
+            val_acc_avg = 0
+
+        # 恢复最佳模型
+        saver.restore(sess, checkpt_file)
+
+        print("Optimization Finished!")
+        print("Training Time=", "{:.5f}".format(time.time() - intial_t))
+        
+        # 测试模型
+        test_start = time.time()
+        ts_size = features.shape[0]
+        ts_step = 0
+        ts_loss = 0.0
+        ts_acc = 0.0
+
+        while ts_step * batch_size < ts_size:
+            loss_value_ts, acc_ts = sess.run([loss, accuracy],
+                feed_dict={
+                    ftr_in: features[ts_step*batch_size:(ts_step+1)*batch_size],
+                    bias_in: biases[ts_step*batch_size:(ts_step+1)*batch_size],
+                    lbl_in: y_test[ts_step*batch_size:(ts_step+1)*batch_size],
+                    msk_in: test_mask[ts_step*batch_size:(ts_step+1)*batch_size],
+                    is_train: False,
+                    attn_drop: 0.0, ffd_drop: 0.0})
+            ts_loss += loss_value_ts
+            ts_acc += acc_ts
+            ts_step += 1
+        
+        # 打印测试结果
+        print('Test loss:', ts_loss/ts_step, '; Test accuracy:', ts_acc/ts_step, '; test time:', time.time()-test_start)
+
+        # 关闭会话
+        sess.close()
